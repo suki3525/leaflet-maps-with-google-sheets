cab3719fdcccf7bd80bca6c8f7303ac6c436f01a
kamazeuci <joseicastrob@gmail.com>
2020-08-18 11:41:32 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,2 +1,2 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/117evxn_YtEgGF3rFvHjJoR8sx4QQtc-tSh75t1B7lhw/edit#gid=0';
 var googleApiKey = 'AIzaSyDXkfplTsdjVrjnrP_U0v_XzM0p5sCbxy0';
